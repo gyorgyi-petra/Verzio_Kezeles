3d573a125042aff10d2fd98d4789375ed2a8e7d2
Györgyi Petra <gy.petra79@gmail.com>
2018-10-20 20:42:32 +0200

    elso feladat:
    Usertől kérjünk be 10 számot.
    Keressük meg, és írjuk ki a legnagyobbat!

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int[] tomb = new int[10];
+
+            for (int i = 0; i < tomb.Length;i++) {
+                Console.WriteLine("Kérem adjon meg az " + i  +  " egész számot!");
+
+                tomb[i] = Int32.Parse(Console.ReadLine());
+
+
+            }
+
+            Console.ReadLine();
+            Console.WriteLine(tomb.Max());
+
+
+        }
+    }
+}
